ebf7646011d1fc7dbb2d7470387478ca79f44b0f
Santy401 <santiprocastellar6@gmail.com>
2025-05-02 12:32:04 -0500

    commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,9 @@
-// Configuracion De PosgreSQL
 import { Sequelize } from 'sequelize';
 
 const DB_NAME = process.env.DB_NAME || 'TasksRios';
-const DB_USER = process.env.DB_USER || 'san'; 
+const DB_USER = process.env.DB_USER || 'san';
 const DB_PASSWORD = process.env.DB_PASSWORD || 'santy401';
-const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_HOST = process.env.DB_HOST || 'localhost'; // Cambia aquí
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
