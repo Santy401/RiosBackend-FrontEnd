617b1bca3d4af197325a49c5c81d18c60a2ae794
Santy401 <santiprocastellar6@gmail.com>
2025-05-31 21:16:18 -0500

    Update Server.js

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,8 @@ const startServer = async () => {
 
     app.use(express.static(path.join(__dirname, "../public")));
 
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+    app.listen(PORT,'0.0.0.0', () => {
+      console.log(`Server running on http://0.0.0.0:${PORT}`);
     });
     
   } catch (error) {
