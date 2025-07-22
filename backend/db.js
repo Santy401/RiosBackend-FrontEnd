0b64589d978e2738a53ecbe31287406c1b256258
Santy401 <santy401@gmail.com>
2025-07-22 18:02:45 -0500

    Quit Backend This Frontend

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-import sequelize from './config/database.js';
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('🟢 Conexión con PostgreSQL exitosa!');
-  })
-  .catch(err => {
-    console.error('🔴 Error al conectar con PostgreSQL:', err);
-  });
--- backend/middleware/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    message: 'Internal Server Error',
-    error: process.env.NODE_ENV === 'development' ? err.message : undefined,
-  });
-};
-
-export default errorHandler;
